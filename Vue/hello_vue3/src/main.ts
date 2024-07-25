5a122559cfde6deb721460cd1121df5381e78b04
thatsShirleyLee <2690233316@qq.com>
2024-07-26 01:19:21 +0800

    update vue

--- Vue/hello_vue3/src/main.ts
+++ Vue/hello_vue3/src/main.ts
@@ -1,6 +1,8 @@
-// 引入 App 用于创建应用
-import {createApp} from "vue";
-// 引入 App 根组件
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 创建应用
+const app = createApp(App)
+
+// 挂载应用
+app.mount('#app')
